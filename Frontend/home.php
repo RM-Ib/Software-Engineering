b28a925046dd928352a335e08fe907fdf078e33a
RM-Ib <ranimibrahim145@gmail.com>
2025-04-23 01:34:55 +0300

    more changes

--- Frontend/home.php
+++ Frontend/home.php
@@ -14,7 +14,6 @@ session_start();
     <nav class="navbar" id="navbar">
         <img src="../Images/Logo.png" alt="academy logo" class="logo">
         <ul class="navbar_items" id="navbar_items">
-<<<<<<< Updated upstream
     <li><a href="home.php">Home</a></li>
 
     <?php if (isset($_SESSION['user_id'])): ?>
@@ -30,33 +29,15 @@ session_start();
         <div class="dropdown-label">Activities ▼</div>
         <div class="activitiesnav">
             <ul>
-                <li><a href="SkiingPage.html">Skiing</a></li>
-                <li><a href="Snowboarding.html">Snowboarding</a></li>
-                <li><a href="Sledding.html">Sledding</a></li>
-                <li><a href="Iceclimbing.html">Ice Climbing</a></li>
+                <li><a href="SkiingPage.php">Skiing</a></li>
+                <li><a href="Snowboarding.php">Snowboarding</a></li>
+                <li><a href="Sledding.php">Sledding</a></li>
+                <li><a href="Iceclimbing.php">Ice Climbing</a></li>
             </ul>
         </div>
     </li>
 </ul>
 
-=======
-            <li><a href="home.php">Home</a></li>
-            <li class="logged_in"><a href="UserProfile.php">My Profile</a></li>
-            <li>
-                <div class="dropdown-label">Activities ▼</div>
-                <div class="activitiesnav">
-                    <ul>
-                        <li><a href="SkiingPage.php">Skiing</a></li>
-                        <li><a href="Snowboarding.php">Snowboarding</a></li>
-                        <li><a href="Sledding.php">Sledding</a></li>
-                        <li><a href="Iceclimbing.php">Ice Climbing</a></li>
-                    </ul>
-                </div>
-            </li>
-            <li class="notLogged_in"><a href="Log-in.php">Login</a></li>
-            <li class="logged_in"><a href="#" id="logoutBtn">Logout</a></li>
-        </ul>
->>>>>>> Stashed changes
     </nav>
     <section class="hero">
         <div class="hero-content">
