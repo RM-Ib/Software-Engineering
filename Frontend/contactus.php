4d682fd1f2602412872a7fbec3dca15cafbcaa5a
Antonio Karam <antonio.karam@lau.edu>
2025-04-23 09:34:26 +0300

    fixed a few things

--- Frontend/contactus.php
+++ Frontend/contactus.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -304,25 +307,34 @@
   <div class="overlay"></div>
 
   <nav class="navbar" id="navbar">
-    <img src="../Images/Logo.png" alt="academy logo" class="logo">
-    <ul class="navbar_items" id="navbar_items">
-      <li><a href="home.php">Home</a></li>
-      <li class="logged_in"><a href="UserProfile.php">My Profile</a></li>
-      <li>
+        <img src="../Images/Logo.png" alt="academy logo" class="logo">
+        <ul class="navbar_items" id="navbar_items">
+    <li><a href="home.php">Home</a></li>
+
+    <?php if (isset($_SESSION['user_id'])): ?>
+        <!-- User is logged in -->
+        <li><a href="UserProfile.php">My Profile</a></li>
+        <li><a href="../Backend/logout.php" id="logoutBtn">Logout</a></li>
+    <?php else: ?>
+        <!-- User is NOT logged in -->
+        <li><a href="Log-in.php">Login</a></li>
+    <?php endif; ?>
+
+    <li>
         <div class="dropdown-label">Activities ▼</div>
         <div class="activitiesnav">
-          <ul>
-            <li><a href="SkiingPage.php">Skiing</a></li>
-            <li><a href="Snowboarding.php">Snowboarding</a></li>
-            <li><a href="Sledding.php">Sledding</a></li>
-            <li><a href="Iceclimbing.php">Ice Climbing</a></li>
-          </ul>
+            <ul>
+                <li><a href="SkiingPage.php">Skiing</a></li>
+                <li><a href="Snowboarding.php">Snowboarding</a></li>
+                <li><a href="Sledding.php">Sledding</a></li>
+                <li><a href="Iceclimbing.php">Ice Climbing</a></li>
+            </ul>
         </div>
-      </li>
-      <li class="notLogged_in"><a href="Log-in.php">Login</a></li>
-      <li class="logged_in"><a href="#" id="logoutBtn">Logout</a></li>
-    </ul>
-  </nav>
+    </li>
+</ul>
+
+    </nav>
+
 
   <main>
     <div class="contact-form">
